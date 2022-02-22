88a199c0fee8cb25f5b586c6cbb02a8173229d68
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2022-02-22 15:33:56 +0100

    🐛 Fix statusbar issue on ios

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
       <RestaurantsScreen />
-      <ExpoStatusBar style='auto' />
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
