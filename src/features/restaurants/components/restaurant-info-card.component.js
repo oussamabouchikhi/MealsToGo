8ab75a5d959c4e4dc416a652b3227d66ff1d0560
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2022-02-22 15:04:10 +0100

    :sparkles: convert styles to using styled-components

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Card } from "react-native-paper";
+
+const RestaurantCard = styled(Card)`
+  background-color: "white";
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  background-color: "white";
+  padding: 20;
+`;
+
+const Title = styled.Text`
+  padding: 16px;
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover
+        key={name}
+        source={{
+          uri: photos[0],
+        }}
+      />
+      <Title>{name}</Title>
+    </RestaurantCard>
+  );
+};
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import { StatusBar, SafeAreaView, Text, View, StyleSheet } from 'react-native';
-import { Searchbar } from 'react-native-paper';
-import { RestaurantInfo } from '../components/restaurant-info.component';
+import React from "react";
+import { StatusBar } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import styled from "styled-components/native";
+
+const SafeArea = styled.SafeAreaView`
+  flex: 1;
+  margin-top: ${StatusBar.currentHeight}px;
+`;
+
+const SearchContainer = styled(Searchbar)`
+  padding: 16px;
+`;
+
+const RestaurantListContainer = styled(Searchbar)`
+  flex: 1;
+  padding: 16px;
+  background-color: blue;
+`;
 
 export const RestaurantsScreen = () => (
-  <SafeAreaView style={styles.container}>
-    <View style={styles.search}>
+  <SafeArea>
+    <SearchContainer>
       <Searchbar />
-    </View>
-    <View style={styles.list}>
-      <RestaurantInfo />
-    </View>
-  </SafeAreaView>
+    </SearchContainer>
+    <RestaurantListContainer>
+      <RestaurantInfoCard />
+    </RestaurantListContainer>
+  </SafeArea>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: 'blue',
-  },
-});
