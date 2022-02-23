90f6402d12df0ca58071e622787b55655694d90d
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2022-02-23 08:48:34 +0100

    🐛 Fix spacer error on Android

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,30 +1,35 @@
-import styled from "styled-components/native";
+import React from "react";
+import styled, { useTheme } from "styled-components/native";
 
 const sizeVariants = {
-	small: 1,
-	medium: 2,
-	large: 3,
-}
+  small: 1,
+  medium: 2,
+  large: 3,
+};
 
 const positionVariants = {
-	top: "marginTop",
-	right: "marginRight",
-	bottom: "marginBottom",
-	left: "marginLeft",
-}
+  top: "marginTop",
+  right: "marginRight",
+  bottom: "marginBottom",
+  left: "marginLeft",
+};
 
 const getVariant = (position, size, theme) => {
   const property = positionVariants[position];
   const sizeIndex = sizeVariants[size];
   const value = theme.space[sizeIndex];
-  return `${property}:${value}`
+  return `${property}:${value}`;
 };
+
+export const SpacerView = styled.View`
+  ${(variant) => variant}
+`;
 
 /**
  * This function creates a space or a margin to be specefic, if for example
  * we pass position="left" and size="medium" it is going to return for us
  * "marginLeft: 8px".
- * - Usage you can either add it as an isolated component <Spacer /> 
+ * - Usage you can either add it as an isolated component <Spacer />
  * or wrap another component <Spacer></Spacer> which better for debugging
  * - Default position="top" size="small"
  * @param {string} position the space position (top, right, bottom, left)
@@ -32,11 +37,13 @@ const getVariant = (position, size, theme) => {
  * @param {object} theme the app theme
  * @return {string} the actual css property (ex: "marginTop: 16px;" )
  */
-export const Spacer = styled.View`
-	${( position, size, theme ) => getVariant(position, size, theme)}
-`
+export const Spacer = ({ position, size, children }) => {
+  const theme = useTheme();
+  const variant = getVariant(position, size, theme);
+  return <SpacerView variant={variant}>{children}</SpacerView>;
+};
 
 Spacer.defaultProps = {
-  position="top",
-  size="small",
-}
+  position: "top",
+  size: "small",
+};
