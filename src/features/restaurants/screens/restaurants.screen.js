88220d625e49ad51842a7d63398636b78d34eff5
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2022-02-24 15:17:39 +0100

    :sparkles: Add Activity Indicator while loading

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
+import { ActivityIndicator, Colors } from "react-native-paper";
 import styled from "styled-components/native";
 
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
@@ -11,6 +12,16 @@ import { SafeArea } from "../../../components/utility/safe-area.component";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
+`;
+
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
 `;
 
 const RestaurantList = styled(FlatList).attrs({
@@ -24,6 +35,11 @@ export const RestaurantsScreen = () => {
   console.log(restaurants);
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={Colors.blue300} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
