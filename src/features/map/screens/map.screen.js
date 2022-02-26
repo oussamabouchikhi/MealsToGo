7aa7a5036e2c27832aacbe41171db73de01dd648
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2022-02-26 09:08:46 +0100

    :sparkles: link callout with restaurant detail

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -11,7 +11,7 @@ const Map = styled(MapView)`
   width: 100%;
 `;
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
   const { location } = useContext(LocationContext);
   const { restaurants = [] } = useContext(RestaurantsContext);
   const [latDelta, setLatDelta] = useState(0);
@@ -44,7 +44,11 @@ export const MapScreen = () => {
               latitude: restaurant.geometry.location.lat,
             }}
           >
-            <MapView.Callout>
+            <MapView.Callout
+              onPress={() =>
+                navigation.navigate("RestaurantDetail", { restaurant })
+              }
+            >
               <MapCallout restaurant={restaurant} />
             </MapView.Callout>
           </MapView.Marker>
