1f87116e361a274aaa97b606eb74dce9576ddb71
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2022-03-05 10:01:21 +0100

    🎨 Add authentication boiler plate

---
+++ src/features/account/screens/login.screen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { AccountBackgroud, AccountCover } from "../components/account.styles";
+
+export const LoginScreen = () => {
+  return (
+    <AccountBackgroud>
+      <AccountCover />
+    </AccountBackgroud>
+  );
+};
---
+++ src/features/account/screens/register.screen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { AccountBackgroud, AccountCover } from "../components/account.styles";
+
+export const RegisterScreen = () => {
+  return (
+    <AccountBackgroud>
+      <AccountCover />
+    </AccountBackgroud>
+  );
+};
